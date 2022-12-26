ddb6aefd81e1061f9a34e603f9d3ac88a9ffe15d
Aditya Sundawa <aditya@sindo.sg>
2022-12-26 18:11:37 +0700

    update26_12

--- input_data.py
+++ input_data.py
@@ -1,4 +1,4 @@
-#input user
+# input user
 # data yang di masukkan pasti string
 
 data = input("Masukkan data: ")
@@ -6,13 +6,12 @@ data = input("Masukkan data: ")
 print("data = ", data, ", type = ", type(data))
 
 
-#jika kita ingin mengambil integer, maka
+# jika kita ingin mengambil integer, maka
 
 data_int = int(input("Masukkan angka : "))
 print("data = ", data_int, ", type = ", type(data_int))
 
 angka = float(input("Masukkan angka : "))
 angka = int(input("Masukkan angka : "))
-   
-print("data = ", angka, ", type = ", type(angka))
 
+print("data = ", angka, ", type = ", type(angka))
